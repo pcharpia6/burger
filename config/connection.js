8f86799a453534b8de24701a0b743ee4be154c1d
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-06-24 21:50:20 -0400

    added jawsDB information

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up the database connection
 var mysql = require("mysql");
 // Create and export the database connection
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: process.env.PORT || 3306,
-  user: "root",
-  password: "IronBreaker0^",
-  database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 3306,
+    user: "root",
+    password: "IronBreaker0^",
+    database: "burgers_db"
+  });
+};
 // Establish connection
 connection.connect(function(err) {
   if (err) {
