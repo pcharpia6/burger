1eb5a7bc523f9c448627383ff7b494d8218a74f4
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-06-24 21:23:46 -0400

    updated port

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 // Create and export the database connection
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "IronBreaker0^",
   database: "burgers_db"
